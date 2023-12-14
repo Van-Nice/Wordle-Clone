dce715bb45cd03ad0cd8adddbc32a4e176d2e22f
Van-Nice <wilsonwalter808@gmail.com>
2023-12-13 19:56:13 -0700

    created all rows, single letter typing/deleting, and hash map for the random word

--- main.py
+++ main.py
@@ -7,58 +7,66 @@ with open(words_path, 'r') as words_file:
     words = [line.strip() for line in words_file]
 
 random_word = random.choice(words)
+random_word_hashm = {}
+# create function that will create a hash map for the word. This hash map will use the letter as it's key and the index position as the value
+for i in range(0, len(random_word)):
+    random_word_hashm[random_word[i]] = i
 
-def guess1():
-    guess = input("Enter a 5 letter word:\n")
-    if len(guess) > 5 or len(guess) < 5 or guess.isalpha() == False:
-        print("Invalid input, must be 5 characters and alphabetic")
-        guess1()
-    return guess
-
-# Graphic User Interface
 root = tk.Tk()
+root.title("Character Grid Input")
 
-root.geometry("500x700")
-root.title("Wordle V2")
+frame = tk.Frame(root, borderwidth=0, relief="groove")
+frame.pack(padx=100, pady=100)
 
-textbox_frame = tk.Frame(root)
-textbox_frame.columnconfigure(0, weight=1)
-textbox_frame.columnconfigure(1, weight=1)
-textbox_frame.columnconfigure(2, weight=1)
-textbox_frame.columnconfigure(3, weight=1)
-textbox_frame.columnconfigure(4, weight=1)
+rows = 6
+columns = 5
+labels = []
 
-# Row 1
-# Box 1
-textbox1 = tk.Text(textbox_frame, height=3, font=('Arial', 16))
-textbox1.grid(row=0, column=0, sticky=tk.W+tk.E)
+for i in range(rows):
+    row = []
+    for j in range(columns):
+        label = tk.Label(frame, text="", height=2, width=4, borderwidth=2.5, relief="solid", font=("Helvetica", 32))
+        label.grid(row=i, column=j, padx=5, pady=5)
+        row.append(label)
+    labels.append(row)
 
-# Box 2
-textbox2 = tk.Text(textbox_frame, height=3, font=('Arial', 16))
-textbox2.grid(row=0, column=1, sticky=tk.W+tk.E)
+current_row, current_col = 0, 0
 
-# Box 3
-textbox3 = tk.Text(textbox_frame, height=3, font=('Arial', 16))
-textbox3.grid(row=0, column=2, sticky=tk.W+tk.E)
+def on_key_press1(event):
+    global current_row, current_col
+    if event.keysym == 'BackSpace':
+        if current_col > 0:
+            current_col -= 1
+        else:
+            return
+        labels[current_row][current_col].config(text="")
+    elif event.char.isalpha():
+        labels[current_row][current_col].config(text=event.char.upper())
+        current_col += 1
 
-# Box 4
-textbox4 = tk.Text(textbox_frame, height=3, font=('Arial', 16))
-textbox4.grid(row=0, column=3, sticky=tk.W+tk.E)
+def get_first_row_letters():
+    first_row_letters = []
+    for label in labels[0]:
+        letter = label.cget("text")
+        if letter:
+            first_row_letters.append(letter)
+    return first_row_letters
 
-# Box 5
-textbox5 = tk.Text(textbox_frame, height=3, font=('Arial', 16))
-textbox5.grid(row=0, column=4, sticky=tk.W+tk.E)
+enter_button = tk.Button(root, text="Enter", command=lambda: get_first_row_letters())
+enter_button.pack()
 
-textbox_frame.pack(fill='x')
 
+# handle first row event
+# take the first row list and iterate to if each letter is the correct position and change it green, if it is in hashm but not in the correct position change it yellow
+# create edge cases for double and triple letters
+
+
+if current_col <= 5 and current_row == 0:
+    root.bind("<Key>", on_key_press1)
+
+labels[0][0].config(foreground="white", background="green")
 
 root.mainloop()
 
-# create game loop
-run = True
-while run:
-    print("Welcome to wordle v2!\nThe rules are the same as wordle you get 6 tries to guess a 5 letter word!\n")
-    guess1()
-    run = False
 
 
